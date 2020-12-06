b6e98b5b69ac3c806f9f9fb90989946e8f0cf01b
swsoyee <swsoyee@gmail.com>
2020-12-06 19:57:41 +0900

    fix: class of each column in calendar table result

--- R/bgm-calendar.R
+++ R/bgm-calendar.R
@@ -2,20 +2,17 @@
 #'
 #' @param format Result will be a list of json data when set to \code{list}
 #' (default), \code{response()} object when set to \code{default}. Set to
-#' \code{table} will convert the episode list to a \code{data.frame} for ease of
-#' use.
+#' \code{table} will convert the list to a \code{data.frame} for ease of use.
 #'
 #' @importFrom httr GET content
+#' @importFrom data.table rbindlist set
 #'
 #' @return A list of response content or a \code{response()} object according to
 #' the \code{format} option.
 #' @export
 #'
 #' @examples
-#' result <- bgm_calendar("table")
-#'
-#' # Bangumi list on Monday
-#' result$mon
+#' bgm_calendar("table")
 bgm_calendar <- function(format = c("list", "table", "default")) {
   # config
   base <- "https://api.bgm.tv/calendar"
@@ -38,11 +35,19 @@ bgm_calendar <- function(format = c("list", "table", "default")) {
       table_list <- lapply(content, function(day) {
         json_to_df(day$items)
       })
-      name_list <- sapply(content, function(day) {
-        tolower(day$weekday$en)
-      })
-      names(table_list) <- name_list
-      table_list
+      table <- data.table::rbindlist(table_list, use.names = TRUE)
+      col_name <- colnames(table)[grepl(
+        "(rating|collection|rank)",
+        colnames(table)
+      )]
+      for (col in col_name) {
+        data.table::set(table, j = col, value = as.numeric(table[[col]]))
+      }
+      for (col in c("type", "air_weekday")) {
+        data.table::set(table, j = col, value = as.factor(table[[col]]))
+      }
+      data.table::set(table, j = "air_date", value = as.Date(table$air_date))
+      table
     }
   ))
 }
--- tests/testthat/test-bgm-calendar.R
+++ tests/testthat/test-bgm-calendar.R
@@ -1,5 +1,6 @@
 test_that("bgm_calendar()", {
-  mockRes <- bgm_calendar(format = "table")
-  expect_type(mockRes, "list")
-  expect_s3_class(mockRes$mon, "data.table")
+  expect_s3_class(
+    bgm_calendar(format = "table"),
+    "data.table"
+  )
 })
